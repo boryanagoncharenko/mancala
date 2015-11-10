4167d250dcad15677144717ef022eb4de04329f4
Boryana <boryana.goncharenko@gmail.com>
2015-11-10 16:11:26 +0100

    updated maven dependencies, so that now Spring Session is storing session corretly in Redis

--- src/main/java/com/tinygames/mancala/HelloController.java
+++ src/main/java/com/tinygames/mancala/HelloController.java
@@ -5,11 +5,15 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequestMapping("/")
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
+	public String printWelcome(ModelMap model, HttpSession session)
+	{
+		session.setAttribute("success" , "successfully accessed");
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
